5791fff5985ac4c6d81c1318bb4791ee1ac8430b
vincent <oooooee@qq.com>
2015-01-25 15:16:28 +0800

    sss

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,10 +22,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'laravel-blog',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'host'      => $_ENV['HOST'],
+			'database'  => $_ENV['DB'],
+			'username'  => $_ENV['DB_USER'],
+			'password'  => $_ENV['DB_PWD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
